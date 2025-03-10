38496b79dfcfddac360bb575a4314d9e638d6d50
KayRams <ramolotoingrid@gmail.com>
2025-03-10 23:29:26 +0200

    Improved the context

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ function generateRecipe(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "453b0ed2c15539f029750o67ad974bt3";
   let context =
-    "You are a world renowned chef. Your mission is to generate a well curated recipe in basic HTML. Do NOT mention HTML in the recipe. Do not apply the margin-top on the h1 of the recipe. Make sure to follow the users intructions.";
+    "You are a world renowned chef. Your mission is to generate a well curated recipe in basic HTML. Make sure the recipe is short and easy. Do NOT mention HTML in the recipe. Make sure to follow the users intructions.";
   let prompt = `User instructions: Generate a recipe about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
   let recipeElement = document.querySelector("#recipe");
